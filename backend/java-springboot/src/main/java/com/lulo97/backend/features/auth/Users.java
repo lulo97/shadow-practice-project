98ba898ff696dfbf31699d7c93c1cc118c3e6e23
luongpysl2 <luongpysl2@gmail.com>
2026-06-29 03:07:48 +0700

    add user model class

--- backend/java-springboot/src/main/java/com/lulo97/backend/features/auth/Users.java
+++ backend/java-springboot/src/main/java/com/lulo97/backend/features/auth/Users.java
@@ -1,5 +1,10 @@
 package com.lulo97.backend.features.auth;
 
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -13,5 +18,13 @@ public class Users {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    
+    @Column(name="username", nullable = false)
+    private String username;
+
+    @Column(name="password_hashed", nullable = false)
+    private String password_hashed;
+
+    @CreationTimestamp
+    @Column(name="created_at", nullable = false)
+    private LocalDateTime created_at;
 }
